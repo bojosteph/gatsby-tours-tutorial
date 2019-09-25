4af18d3fced424a1d1405d6d1ddd0e89f83a1375
Roberto Samia <bojosteph@gmail.com>
2019-09-24 22:25:50 -0400

    Created Gatsby application and added components and navbar

---
+++ src/pages/blog.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+
+export class blog extends Component {
+  render() {
+    return <div>this is our blog pages</div>
+  }
+}
+
+export default blog
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
+import Navbar from "../components/Navbar"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <Navbar />
+    <h1>Thus is our home page</h1>
+    Hello world!
+  </div>
+)
